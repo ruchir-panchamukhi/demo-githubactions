dfedeed20735848eb2788cbd7aa82a3dcea5e628
Ruchir Panchamukhi <ruchir.panchamukhi@TL213.triconhome.local>
2024-10-28 14:30:56 +0530

    made changes in the setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='demo-package',  # Your package name
     version='0.1.0',  # Package version
-    author='Your Name',  # Your name
-    author_email='your.email@example.com',  # Your email
+    author='Ruchir',  # Your name
+    author_email='ruchir.panchamukhi@Triconinfotech.com',  # Your email
     description='A simple example package',  # Short description
     long_description=open('README.md').read(),  # Long description
     long_description_content_type='text/markdown',
-    url='https://github.com/ruchir-panchamukhi/demo-githubactions',  # Repo URL
+    url='https://github.com/ruchir-panchamukhi/demo-githubactions.git',  # Repo URL
     packages=find_packages(),  # Automatically find packages
     classifiers=[
         'Programming Language :: Python :: 3',
