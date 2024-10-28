e67b13303efc8991fe3df46711a01d6b8f438958
Ruchir Panchamukhi <ruchir.panchamukhi@TL213.triconhome.local>
2024-10-28 12:36:19 +0530

    added a test case

--- test_main.py
+++ test_main.py
@@ -1,6 +1,10 @@
 # test_main.py
-from main import add
+from main import add, sub
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+def test_sub():
+    assert sub(2, 3) == -1
+    assert sub(4, 5) == 2
